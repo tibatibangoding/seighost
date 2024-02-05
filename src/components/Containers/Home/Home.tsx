982fd1cd45dddf22f66b8740896481c79d42f482
Naufal Akbar <naufalakbar378@gmail.com>
2024-02-05 19:30:47 +0700

    init

---
+++ src/components/Containers/Home/Home.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+
+import MainLayout from '@/layouts/MainLayout';
+
+const ContainerHome: FC = () => {
+  return (
+    <>
+      <MainLayout>
+        <section className="max-w-7xl mx-auto">
+          <div>
+            <div className="flex flex-wrap">
+              <div className="w-full px-4">
+                <h1 className="text-4xl font-bold text-center text-gray-800">
+                  aaaaaaaaaaaaaaaaaaaaaaa
+                </h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      </MainLayout>
+    </>
+  );
+};
+
+export default ContainerHome;
