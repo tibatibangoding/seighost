d365c34ad0a5675c0ab4d80ab8a17d3946e297b3
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-02-08 14:41:55 +0700

    update

--- src/components/Containers/Home/Home.tsx
+++ src/components/Containers/Home/Home.tsx
@@ -1,23 +1,339 @@
-import { FC } from 'react';
+'use client';
+import { FC, useState, useEffect } from 'react';
 
-import MainLayout from '@/layouts/MainLayout';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
 
 const ContainerHome: FC = () => {
+  const [scale, setScale] = useState<number>(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const newScale = 1 + scrollY * 0.0001; // Sesuaikan faktor skala sesuai kebutuhan Anda
+      setScale(newScale);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <MainLayout>
-        <section className="max-w-7xl mx-auto">
-          <div>
-            <div className="flex flex-wrap">
-              <div className="w-full px-4">
-                <h1 className="text-4xl font-bold text-center text-gray-800">
-                  aaaaaaaaaaaaaaaaaaaaaaa
-                </h1>
+      <div className="h-screen fixed -z-50">
+        <Image
+          src="/images/pxfuel.jpg"
+          alt="Picture of the author"
+          width={10000}
+          height={0}
+          className="transform transition duration-500 "
+        />
+      </div>
+      <section className="w-full h-screen">
+        <div className="relative w-full h-full" style={{ overflow: 'hidden' }}>
+          <Image
+            src="/images/bg-seivatars.png"
+            alt="Picture of the author"
+            width={10000}
+            height={0}
+            className="transform transition duration-500 object-cover "
+            style={{
+              transform: `scale(${scale})`,
+              transition: 'transform 0.15s ease-in-out',
+            }}
+          />
+          <Image
+            src={'/images/hero.gif'}
+            width={300}
+            height={300}
+            alt="hero"
+            className="absolute bottom-0 left-0"
+          />
+        </div>
+      </section>
+      <section className="flex flex-col gap-4 mt-4 ">
+        <div className="">
+          <Swiper
+            breakpoints={{
+              375: {
+                // width: 375,
+                slidesPerView: 2,
+              },
+              576: {
+                // width: 576,
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 8,
+              },
+            }}
+            spaceBetween={30}
+            autoplay={{
+              delay: 0,
+            }}
+            speed={2000}
+            loop={true}
+            modules={[Autoplay]}
+            className="mySwiper "
+          >
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
               </div>
-            </div>
-          </div>
-        </section>
-      </MainLayout>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl">
+                <video
+                  controls
+                  loop
+                  autoplay
+                  playsInline
+                  className="object-cover"
+                >
+                  <source src="/video/7.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="w-full" dir="rtl">
+          <Swiper
+            breakpoints={{
+              375: {
+                // width: 375,
+                slidesPerView: 2,
+              },
+              576: {
+                // width: 576,
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 8,
+              },
+            }}
+            spaceBetween={30}
+            autoplay={{
+              delay: 0,
+            }}
+            speed={2000}
+            loop={true}
+            modules={[Autoplay]}
+            className="mySwiper "
+          >
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[200px] h-[200px] bg-red-500 rounded-xl"></div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </section>
+      {/* <section className="w-full  mt-4"></section> */}
     </>
   );
 };
