b9d11eafdaa07af1ce3a6de5479119a6923b644b
Naufal Akbar <naufalakbar378@gmail.com>
2024-02-19 12:10:46 +0700

    update

--- src/components/Common/ScrollToTop.tsx
+++ src/components/Common/ScrollToTop.tsx
@@ -38,7 +38,7 @@ const ScrollToTop: FC = () => {
           onClick={scrollTop}
           className={classNames(
             isVisible ? 'opacity-100' : 'opacity-0 cursor-default',
-            'flex flex-col justify-center items-center rounded-lg bg-teal-500 p-2 text-white transition-all hover:bg-teal-600'
+            'flex flex-col justify-center items-center rounded-lg bg-teal-500 p-2 text-white transition-all hover:bg-teal-600',
           )}
         >
           <svg
--- src/components/Containers/Home/Home.tsx
+++ src/components/Containers/Home/Home.tsx
@@ -40,7 +40,7 @@ const ContainerHome: FC = () => {
 
   const handleToggleAccordion = (accordionNumber: number) => {
     setOpenAccordion((prev) =>
-      prev === accordionNumber ? null : accordionNumber
+      prev === accordionNumber ? null : accordionNumber,
     );
   };
 
